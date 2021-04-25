a0fcf25bd4c16a10a4183df543fd0d645368c320
gaonn <gfsrq453@gmail.com>
2021-04-26 00:06:40 +0900

    Update op_delete.py

--- op_delete.py
+++ op_delete.py
@@ -1,5 +1,5 @@
 import os
-file_path = "C:\Program Files (x86)\Origin Games\Apex\media\respawn.bik"
+file_path = "C:\\Program Files (x86)\\Origin Games\\Apex\\media\\respawn.bik"
 
 if(os.path.isfile(file_path) == True):
 	os.remove(file_path)
